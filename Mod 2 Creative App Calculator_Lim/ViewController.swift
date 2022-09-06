709c3fac536a3d02607ccca9949a6f3c3db088b8
Samuel Lim <samuel.lim23@d155.org>
2022-09-06 07:24:41 -0500

    Initial Commit

---
+++ Mod 2 Creative App Calculator_Lim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod 2 Creative App Calculator_Lim
+//
+//  Created by SAMUEL LIM on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
