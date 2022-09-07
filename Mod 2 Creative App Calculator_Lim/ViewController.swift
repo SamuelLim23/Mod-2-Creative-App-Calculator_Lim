c2d27aac0f7dd45773a5d8b73eb145f65a47e088
Samuel Lim <samuel.lim23@d155.org>
2022-09-07 07:56:38 -0500

    Initial Commit

--- Mod 2 Creative App Calculator_Lim/ViewController.swift
+++ Mod 2 Creative App Calculator_Lim/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputOne: UITextField!
+    @IBOutlet weak var inputTwo: UITextField!
+    
+    @IBOutlet weak var output: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func multiply(_ sender: Any) {
+        if let unwrappedOne = Double(inputOne.text ?? "0"){
+            if let unwrappedTwo = Double(inputTwo.text ?? "0"){
+                output.text = String(unwrappedOne * unwrappedTwo)
+            }
+        }
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        if let unwrappedOne = Double(inputOne.text ?? "0"){
+            if let unwrappedTwo = Double(inputTwo.text ?? "0"){
+                output.text = String(unwrappedOne / unwrappedTwo)
+            }
+        }
+    }
+    
+    
+    @IBAction func subtract(_ sender: Any) {
+        if let unwrappedOne = Double(inputOne.text ?? "0"){
+            if let unwrappedTwo = Double(inputTwo.text ?? "0"){
+                output.text = String(unwrappedOne - unwrappedTwo)
+            }
+        }
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        if let unwrappedOne = Double(inputOne.text ?? "0"){
+            if let unwrappedTwo = Double(inputTwo.text ?? "0"){
+                output.text = String(unwrappedTwo + unwrappedOne)
+            }
+        }
+    }
+    
+    @IBAction func perimeter(_ sender: Any) {
+        if let unwrappedOne = Double(inputOne.text ?? "0"){
+            if let unwrappedTwo = Double(inputTwo.text ?? "0"){
+                output.text = String((unwrappedTwo * 2.0) + (unwrappedOne * 2.0))
+            }
+        }
+    }
+    
+    @IBAction func hypotenuse(_ sender: Any) {
+        if let unwrappedOne = Double(inputOne.text ?? "0"){
+            if let unwrappedTwo = Double(inputTwo.text ?? "0"){
+                output.text = String(sqrt(pow(unwrappedOne, 2.0) + pow(unwrappedTwo, 2.0)))
+            }
+        }
+    }
+    
 }
 
